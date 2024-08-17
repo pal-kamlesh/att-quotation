0e297b2a64d5e09243912a80e1523b8c7f2183d8
pal-kamlesh <kamleshiit5@gmail.com>
2024-08-17 10:38:27 +0530

    simple change

--- controller/quoteController.js
+++ controller/quoteController.js
@@ -436,7 +436,6 @@ const getArchive = async (req, res, next) => {
         populate: { path: "revisions.author", model: "User" },
       })
       .lean({ virtuals: ["subject"] });
-    console.log(data);
     res.status(200).json({
       message: "Nothing to say for now.",
       result: data,
