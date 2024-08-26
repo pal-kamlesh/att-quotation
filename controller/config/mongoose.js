b0a3c5119e8b676856d11be97c5df7b104ab732d
pal-kamlesh <kamleshiit5@gmail.com>
2024-08-26 15:28:46 +0530

    on till cards, wokrlogform

--- controller/config/mongoose.js
+++ controller/config/mongoose.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_LOCAL);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
